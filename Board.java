1ca3e5a828bf87c97e85a559842e4da20b4f4e1c
Jun Dang <jun.dang8@gmail.com>
2016-01-26 11:21:09 -0500

    first commit

---
+++ Board.java
@@ -0,0 +1,136 @@
+import edu.princeton.cs.algs4.Queue;
+
+public class Board {
+    private int N;
+    private int[][] blocks;
+    private int row0, column0;
+    public Board(int[][] blocks) { //construct a board from an N-by-N array of blocks.
+        N = blocks.length;
+        this.blocks = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                this.blocks[i][j] = blocks[i][j];
+                if (blocks[i][j] == 0) {
+                    row0 = i;
+                    column0 = j;
+                    
+                }
+            }
+        }
+    }
+    public int dimension() {
+        return N;
+    }
+    public int hamming() {
+        int hamming = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                /*if ((i == N - 1) && (j == N - 1)) {
+                    if (blocks[i][j] != 0) {
+                        hamming++;
+                    }*/
+                
+                if ((blocks[i][j] != 0) && (blocks[i][j]) != (i * N + j + 1)) {
+                    hamming++;
+                }
+                
+            }
+        }
+        return hamming;
+    }
+    public int manhattan() {
+        int manhattan = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if ((blocks[i][j] != 0) && (blocks[i][j]) != (i * N + j + 1)) {
+                    int temp = blocks[i][j] - 1;
+                    int row = temp / N;
+                    int column = temp % N;
+                    int diff1 = i - row;
+                    if (diff1 < 0) diff1 = -diff1;
+                    int diff2 = j - column;
+                    if (diff2 < 0) diff2 = -diff2;
+                    manhattan = manhattan + diff1 + diff2;
+                }
+            }
+        }
+        return manhattan;
+    }
+    public boolean isGoal() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if ((i == N - 1) && (j == N - 1)) {
+                    if (blocks[i][j] != 0) {
+                        return false;
+                    }
+                } else {
+                    if ((blocks[i][j]) != (i * N + j + 1)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    public Board twin() {
+        int rowNum = (row0 +  1) % N;
+        return exchangeTile(rowNum, 0, rowNum, 1);
+        
+    }
+    private Board exchangeTile(int r1, int c1, int r2, int c2) {
+        int[][] neighbor =  new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                neighbor[i][j] = blocks[i][j];
+            }
+        }
+        int temp = neighbor[r1][c1];
+        neighbor[r1][c1] = neighbor[r2][c2];
+        neighbor[r2][c2] = temp;
+        return new Board(neighbor);
+    }
+    public boolean equals(Object y) {
+        if (this == y) return true;
+        if (y == null) return false;
+        if (this.getClass() != y.getClass()) return false;
+        Board that = (Board) y;
+        if (N != that.dimension()) return false;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (blocks[i][j] != that.blocks[i][j]) return false;
+            }
+        }
+        return true;
+    }
+    public Iterable<Board> neighbors() {
+        Queue<Board> neighbors = new Queue<Board>();
+        if (row0 >= 1) {
+            neighbors.enqueue(exchangeTile(row0 - 1, column0, row0, column0));
+        }
+        if (row0 <= N - 2) {
+            neighbors.enqueue(exchangeTile(row0 + 1, column0, row0, column0));
+        }
+        if (column0 >= 1)
+           {
+              neighbors.enqueue(exchangeTile(row0, column0 - 1, row0, column0));
+           }
+        if (column0 <= N - 2)
+           {
+              neighbors.enqueue(exchangeTile(row0, column0 + 1, row0, column0));
+           }
+            
+        return neighbors;
+        
+    }
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(N + "\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                s.append(String.format("%2d ", blocks[i][j]));
+            }
+              s.append("\n");
+        }
+        return s.toString();        
+    }
+}
---
+++ Solver.java
@@ -0,0 +1,70 @@
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import java.util.Iterator;
+
+public class Solver {
+    private Stack<Board> stack = new Stack<Board>();
+    private boolean isSolvable = false;
+    private class Node implements Comparable<Node> {
+        private Board board;
+        private int move;
+        private int priority;
+        private Node previous;
+        
+        public Node(Board board, int move, Node previous) {
+            this.board = board;
+            this.move = move;
+            this.previous = previous;
+            this.priority = move + board.manhattan();
+        }
+        public int compareTo(Node that) {
+            if (this.priority > that.priority) return 1;
+            if (this.priority < that.priority) return -1;
+            return 0;
+            
+        }
+    }
+    public Solver(Board initial) {
+        MinPQ<Node> pq = new MinPQ<Node>();
+        Node node = new Node(initial, 0, null);
+        pq.insert(node);
+        while (!node.board.isGoal() && !node.board.twin().isGoal()) {
+            node = pq.delMin();
+            Iterator<Board> iter = node.board.neighbors().iterator();
+            while (iter.hasNext()) {
+                Board neighbor = iter.next();
+                if ((node.previous == null) || (!node.previous.board.equals(neighbor))) {
+                    pq.insert(new Node(neighbor, node.move +  1, node));
+                }
+            }
+        }
+        if (node.board.isGoal()) {
+            isSolvable = true;
+            stack = new Stack<Board>();
+            while (node != null) {
+                stack.push(node.board);
+                node = node.previous;
+            }
+        } else {
+            isSolvable = false;
+        }
+        
+    }
+     public boolean isSolvable() {
+         return isSolvable;
+     }
+     public int moves() {
+         return stack.size() - 1;
+     }
+     public Iterable<Board> solution() {
+         if (!isSolvable) return null;
+         Queue<Board> res = new Queue<Board>();
+         Iterator<Board> iter = stack.iterator();
+         while (iter.hasNext()) {
+             res.enqueue(iter.next());
+         }
+         return res;
+     }
+       
+}
